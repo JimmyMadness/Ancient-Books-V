54d155c2bb7e37d88447113220209a76b4860e61
Fabio <fabiobertozzi96@gmail.com>
2018-08-07 23:23:26 +0200

    Small changes

--- src/model/Item.java
+++ src/model/Item.java
@@ -7,4 +7,34 @@ public abstract class Item {
 	protected double weigth;
 	protected ItemID id;
 	
+	public Item(String name, String description, int value, double weigth, ItemID id) {
+		this.name = name;
+		this.description = description;
+		this.value = value;
+		this.weigth = weigth;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public double getWeigth() {
+		return weigth;
+	}
+
+	public ItemID getId() {
+		return id;
+	}
+	
+	
+	
 }
